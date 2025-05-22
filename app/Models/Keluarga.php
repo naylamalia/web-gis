fa48c2e8fdf24b962bdcc222d8775bc64bb746d2
Mirandaa <mirandacvh27@gmail.com>
2025-05-22 13:42:34 +0700

    keluarga model dan controller

--- app/Models/Keluarga.php
+++ app/Models/Keluarga.php
@@ -1,10 +1,40 @@
 <?php
 
+
 namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Keluarga extends Model
 {
-    //
+    use HasFactory;
+
+
+    protected $fillable = [
+        'nama_kepala_keluarga',
+        'alamat',
+        'latitude',
+        'longitude',
+        'rt',
+        'rw',
+        'desa',
+        'kecamatan',
+        'kategori_kemiskinan',
+        'bantuan',
+    ];
+
+
+    // Relasi: satu keluarga memiliki banyak anggota
+
+
+
+
+    public function anggotaKeluarga()
+    {
+        return $this->hasMany(\App\Models\AnggotaKeluarga::class, 'keluarga_id');
+    }
 }
+
